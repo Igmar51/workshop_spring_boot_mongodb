a3941103ce436cbfffa63501b20ebdaf8beebc4b
Igmar Dias <igmar1dias@gmail.com>
2025-12-02 16:25:14 -0300

    Primeiro commit - projeto criado

---
+++ workshopmongo/src/test/java/com/_igmar/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com._igmar.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
