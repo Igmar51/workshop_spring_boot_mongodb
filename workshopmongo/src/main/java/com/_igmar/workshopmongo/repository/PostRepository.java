5ec9739f11aab8c78cbc46ef7f6847ea450ec180
Igmar Dias <igmar1dias@gmail.com>
2025-12-11 16:25:25 -0300

    Chamada meto manual

--- workshopmongo/src/main/java/com/_igmar/workshopmongo/repository/PostRepository.java
+++ workshopmongo/src/main/java/com/_igmar/workshopmongo/repository/PostRepository.java
@@ -7,6 +7,7 @@ package com._igmar.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com._igmar.workshopmongo.domain.Post;
@@ -14,6 +15,9 @@ import com._igmar.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
 
