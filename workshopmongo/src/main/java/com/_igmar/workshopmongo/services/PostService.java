a13ad6ee57cb280d41380c74f7d1838cd84574a2
Igmar Dias <igmar1dias@gmail.com>
2025-12-11 10:31:20 -0300

    Obitendo post por id

---
+++ workshopmongo/src/main/java/com/_igmar/workshopmongo/services/PostService.java
@@ -0,0 +1,27 @@
+package com._igmar.workshopmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com._igmar.workshopmongo.domain.Post;
+import com._igmar.workshopmongo.domain.User;
+import com._igmar.workshopmongo.repository.PostRepository;
+import com._igmar.workshopmongo.services.exception.ObjecNotFoundException;
+
+@Service 
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	
+	
+	public Post findById(String id) {
+	
+	if(id == null) {
+	 throw new IllegalArgumentException("ID não pode ser nulo");
+	}
+	return repo .findById(id).orElseThrow(() -> new ObjecNotFoundException("Objeto não encotrado"));
+	}
+}
+
